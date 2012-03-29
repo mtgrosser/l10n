3025bc2d3a55e2365ef56ffd5b6f1ec1e4e92425
mtgrosser <mtgrosser@gmx.net>
2012-03-29 14:12:19 +0200

    Use bundle exec for cruise

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,7 +1,7 @@
 # Project-specific configuration for CruiseControl.rb
 
 Project.configure do |project|
-  project.build_command = 'rbfu @1.9.3 rake'
+  project.build_command = 'rbfu @1.9.3 bundle exec rake'
   project.scheduler.polling_interval = 30.minutes
   # Set any args for bundler here
   # Defaults to '--path=#{project.gem_install_path} --gemfile=#{project.gemfile} --no-color'
