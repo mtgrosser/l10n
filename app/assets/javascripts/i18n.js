d7c94101880d95bfac60c4d045cde0ad126d8f3a
Matthias Grosser <mtgrosser@gmx.net>
2018-06-21 16:39:07 +0200

    Removing jQuery dependency

--- app/assets/javascripts/i18n.js
+++ app/assets/javascripts/i18n.js
@@ -7,14 +7,23 @@ I18n.translate = function(str, options) {
   var count = options['count'];
   var keys = str.split('.');
   var obj = this.translations;
+  
+  var isString = function(value) {
+    return typeof value === 'string' || value instanceof String;
+  };
+  
+  var isObject = function(value) {
+    return value && typeof value === 'object' && value.constructor === Object;
+  };
+  
   while(obj && keys.length) obj = obj[keys.shift()];
   if (obj) {
-    if ($.type(obj) == 'object' && count !== undefined) {
+    if (isObject(obj) && count !== undefined) {
       translation = obj[1 == count ? 'one' : 'other'];
     } else {
       translation = obj;
     }
-    if ($.type(translation) == 'string' && (matches = translation.match(/{\w+?}/g))) {
+    if (isString(translation) && (matches = translation.match(/{\w+?}/g))) {
       while(match = matches.shift()) {
         method = match.substr(1, match.length - 2);
         interpolation_value = options[method];
